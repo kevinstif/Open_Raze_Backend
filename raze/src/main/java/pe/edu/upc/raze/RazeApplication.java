3e817d00013c63bf0ce7f7f2631c8e8e3763982b
kevinstif <67239585+kevinstif@users.noreply.github.com>
2021-10-30 18:36:46 -0500

    first commit

---
+++ raze/src/main/java/pe/edu/upc/raze/RazeApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.raze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RazeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RazeApplication.class, args);
+	}
+
+}
---
+++ raze/src/test/java/pe/edu/upc/raze/RazeApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.raze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RazeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
