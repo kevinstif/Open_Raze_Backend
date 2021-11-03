8c6f123931fe8cb24e869cac02e1540a668fea76
kevinstif <67239585+kevinstif@users.noreply.github.com>
2021-11-03 11:54:46 -0500

    Merge with Fashions

--- raze/src/main/java/pe/edu/upc/raze/consultancies/mapping/MappingConfiguration.java
+++ raze/src/main/java/pe/edu/upc/raze/consultancies/mapping/MappingConfiguration.java
@@ -2,7 +2,6 @@ package pe.edu.upc.raze.consultancies.mapping;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import pe.edu.upc.raze.consultancies.domain.model.entity.Outfit;
 
 @Configuration("consultanciesMappingConfiguration")
 public class MappingConfiguration {
--- raze/src/main/java/pe/edu/upc/raze/users/interests/mapping/MappingConfiguration.java
+++ raze/src/main/java/pe/edu/upc/raze/users/interests/mapping/MappingConfiguration.java
@@ -1,8 +1,9 @@
 package pe.edu.upc.raze.users.interests.mapping;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration("PostMappingConfiguration")
+@Configuration("UserMappingConfiguration")
 public class MappingConfiguration {
     @Bean
     public InterestMapper interestMapper(){ return new InterestMapper(); }
