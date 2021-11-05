f91fd7c7f4b0d0dfa2e40db7ae7503d11d778f09
kevinstif <67239585+kevinstif@users.noreply.github.com>
2021-11-04 21:10:55 -0500

    Merge with OutfitType

--- raze/src/main/java/pe/edu/upc/raze/consultancies/outfiType/mapping/MappingConfiguration.java
+++ raze/src/main/java/pe/edu/upc/raze/consultancies/outfiType/mapping/MappingConfiguration.java
@@ -3,7 +3,7 @@ package pe.edu.upc.raze.consultancies.outfiType.mapping;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration("consultanciesMappingConfiguration")
+@Configuration("consultanciesOutfitTypesMappingConfiguration")
 public class MappingConfiguration {
 
     @Bean
