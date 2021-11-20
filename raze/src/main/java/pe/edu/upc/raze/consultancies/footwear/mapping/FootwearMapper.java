510d5f9447d1546dac70036018d8358fd44e25f0
U201726085 <u201726085@upc.edu.pe>
2021-11-20 17:22:59 -0500

    Crud Footwear

---
+++ raze/src/main/java/pe/edu/upc/raze/consultancies/footwear/mapping/FootwearMapper.java
@@ -0,0 +1,37 @@
+package pe.edu.upc.raze.consultancies.footwear.mapping;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import pe.edu.upc.raze.consultancies.footwear.domain.model.entity.FootwearModel;
+import pe.edu.upc.raze.consultancies.footwear.resource.CreateFootwearResource;
+import pe.edu.upc.raze.consultancies.footwear.resource.FootwearResource;
+import pe.edu.upc.raze.consultancies.footwear.resource.UpdateFootwearResource;
+import pe.edu.upc.raze.shared.mapping.EnhancedModelMapper;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class FootwearMapper implements Serializable {
+   @Autowired
+    EnhancedModelMapper mapper;
+
+    public FootwearResource toResource(FootwearModel model){
+        return mapper.map(model,FootwearResource.class);
+    }
+
+    public Page<FootwearResource> modelListToPage (List<FootwearModel> modelList, Pageable pageable){
+        return new PageImpl<>(mapper.mapList(modelList, FootwearResource.class), pageable, modelList.size());
+    }
+
+    public FootwearModel toModel (CreateFootwearResource resource){
+        return mapper.map(resource, FootwearModel.class);
+    }
+
+    public FootwearModel toModel (UpdateFootwearResource resource){
+        return mapper.map( resource, FootwearModel.class);
+    }
+
+
+}
--- raze/src/main/java/pe/edu/upc/raze/users/professions/mapping/MappingConfiguration.java
+++ raze/src/main/java/pe/edu/upc/raze/users/professions/mapping/MappingConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 public class MappingConfiguration {
     @Bean
     public ProfessionMapper professionMapperMapper() {
+
         return new ProfessionMapper();
     }
 }
